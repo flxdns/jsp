073ada28af0b73a8201abcb7eb4cc5522d427671
Léo Cartel <leo76930@gmail.com>
2018-04-05 17:26:12 +0200

    age

---
+++ WEB-INF/classes/cmd/epreuves/CommandeSupprimerEpreuve.java
@@ -0,0 +1,36 @@
+package cmd.epreuves;
+
+import bdd.DBS;
+import bdd.DB_EPREUVE;
+import bdd.DB_INSCRIPTION;
+import beans.Epreuve;
+import cmd.Commande;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class CommandeSupprimerEpreuve implements Commande
+{
+	private final String next;
+
+	public CommandeSupprimerEpreuve(String next )
+	{
+		this.next = next;
+	}
+
+	public String execute(HttpServletRequest paramHttpServletRequest) throws Exception
+	{
+		DBS dbs = DBS.getInstance();
+		DB_EPREUVE dbe = dbs.getDB_EPREUVE();
+		DB_INSCRIPTION dbi = dbs.getDB_INSCRIPTION();
+
+		ArrayList alInscription = dbi.getInscriptions();
+		ArrayList alEpreuve = dbe.getEpreuves();
+
+		paramHttpServletRequest.setAttribute("epreuves", alEpreuve);
+		paramHttpServletRequest.setAttribute("inscriptions", alInscription);
+
+		return next;
+	}
+}
