338cfa18ebd5a583060d1d083b787952019cbb1a
Félix DENIS <felix.denis@outlook.com>
2018-04-05 16:06:36 +0200

    .class

--- WEB-INF/classes/cmd/epreuves/CommandeAjoutEpreuve.java
+++ WEB-INF/classes/cmd/epreuves/CommandeAjoutEpreuve.java
@@ -2,23 +2,30 @@ package cmd.epreuves;
 
 import bdd.DBS;
 import bdd.DB_EPREUVE;
+import beans.Epreuve;
 import cmd.Commande;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Date;
 
-public class CommandeAjoutEpreuve implements Commande {
+public class CommandeAjoutEpreuve implements Commande
+{
     private final String next;
-
-    public CommandeAjoutEpreuve(String next) {
+    
+    public CommandeAjoutEpreuve( String next )
+    {
         this.next = next;
     }
-
-    public String execute(HttpServletRequest paramHttpServletRequest) throws Exception {
-        DBS dbs = DBS.getInstance();
-        DB_EPREUVE db_participant = dbs.getDB_EPREUVE();
-
-        //        int idp paramHttpServletRequest.getAttribute("idp");
-//		db_participant.insertEpreuve( new Participant( paramHttpServletRequest.getParameterValues(  ) ) );
+    
+    public String execute( HttpServletRequest paramHttpServletRequest ) throws Exception
+    {
+        DBS        dbs            = DBS.getInstance( );
+        DB_EPREUVE db_participant = dbs.getDB_EPREUVE( );
+        
+        db_participant.insertEpreuve(
+                new Epreuve( 0 , paramHttpServletRequest.getParameter( "nom" ) , paramHttpServletRequest.getParameter( "categ" ) ,
+                             Date.valueOf( paramHttpServletRequest.getParameter( "datep" ) ) ,
+                             Integer.parseInt( paramHttpServletRequest.getParameter( "tarifclub" ) ) , Integer.parseInt( "tarifnonclub" ) ) );
         return next;
     }
 }
